c5cf6501d87e48493bc4f3f83676b8337bc918bc
Ashley <28584443+tank-wsp@users.noreply.github.com>
2019-05-29 22:09:04 -0400

    Created the Scanner and initiated the variables M and N

---
+++ diceCup.java
@@ -0,0 +1,19 @@
+package diceCup;
+import java.util.Scanner;
+public class diceCup {
+	
+	public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+	System.out.println("Please enter two intergers each between 4 and 20: ");
+	int M = input.nextInt();
+	int N = input.nextInt();
+	
+
+	
+
+
+
+
+
+}
+}
